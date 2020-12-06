bd059b3db58edb7a6ef623a30bb8a08de274823c
afazeez <afazeez82@gmail.com>
2020-12-05 21:56:51 -0600

    Updates

---
+++ precache-manifest.9ffc4e05a03e48bbee8c3db032a21d4d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40f27ad1587c6d2c9d83ab6e98c6dae",
+    "url": "/myReactPortfolio-githubio.com/index.html"
+  },
+  {
+    "revision": "a30bddd355e38bb9d796",
+    "url": "/myReactPortfolio-githubio.com/static/css/main.f9400e96.chunk.css"
+  },
+  {
+    "revision": "08f26d9921a19ce98af7",
+    "url": "/myReactPortfolio-githubio.com/static/js/2.313d782e.chunk.js"
+  },
+  {
+    "revision": "0d379520bf19be6441d9b3fd72145865",
+    "url": "/myReactPortfolio-githubio.com/static/js/2.313d782e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a30bddd355e38bb9d796",
+    "url": "/myReactPortfolio-githubio.com/static/js/main.088009bf.chunk.js"
+  },
+  {
+    "revision": "c637bbfb70fe2f1e45a3",
+    "url": "/myReactPortfolio-githubio.com/static/js/runtime-main.23a16164.js"
+  },
+  {
+    "revision": "62a3b4c5bdbe242c3a47b452393b01fa",
+    "url": "/myReactPortfolio-githubio.com/static/media/HouseOptimizer_screenshot.62a3b4c5.png"
+  },
+  {
+    "revision": "19cd1876b2970e14f01a3c267cca552b",
+    "url": "/myReactPortfolio-githubio.com/static/media/Quiz.19cd1876.jpg"
+  },
+  {
+    "revision": "397623792af4f9f70a0680f335b58f28",
+    "url": "/myReactPortfolio-githubio.com/static/media/WorkdayPlanner.39762379.jpg"
+  },
+  {
+    "revision": "820199c73f47eeecc8bc517535973e06",
+    "url": "/myReactPortfolio-githubio.com/static/media/corefactors.820199c7.jpg"
+  },
+  {
+    "revision": "ca120b9ce937f48373b94bbff1d7a8af",
+    "url": "/myReactPortfolio-githubio.com/static/media/myphoto_c copy.ca120b9c.jpg"
+  },
+  {
+    "revision": "5deefe8eb961c79ad588edfe29c70a8e",
+    "url": "/myReactPortfolio-githubio.com/static/media/optum_image.5deefe8e.jpg"
+  },
+  {
+    "revision": "f708e1474f87c9456220bf0bd6e05186",
+    "url": "/myReactPortfolio-githubio.com/static/media/optumimage.f708e147.jpeg"
+  },
+  {
+    "revision": "6d2cfc699bf76a8fa80af86ee7251ed0",
+    "url": "/myReactPortfolio-githubio.com/static/media/optumimagethree.6d2cfc69.jpg"
+  },
+  {
+    "revision": "bc5ecdfe579410d68bd3190ab6132b1c",
+    "url": "/myReactPortfolio-githubio.com/static/media/primetherapeutics.bc5ecdfe.jpg"
+  },
+  {
+    "revision": "dfbf6b362987f375ff1e1023780f89fc",
+    "url": "/myReactPortfolio-githubio.com/static/media/readmegenerator.dfbf6b36.png"
+  },
+  {
+    "revision": "5312c56e7b25f1574e67bdd99611cf58",
+    "url": "/myReactPortfolio-githubio.com/static/media/weatherdashboard.5312c56e.jpg"
+  },
+  {
+    "revision": "8d093f7b7f03509d3c589079644c0731",
+    "url": "/myReactPortfolio-githubio.com/static/media/worldwide.8d093f7b.jpg"
+  }
+]);
